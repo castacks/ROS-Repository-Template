0c7e1c03aa7460a210524a343e0e73261baf28c3
Tom Notch <topnotchymk@gmail.com>
2024-11-18 12:13:20 -0500

    rename run.sh to avoid misunderstanding

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -2,7 +2,7 @@
 
 . "$(dirname "$0")"/variables.sh
 
-. "$(dirname "$0")"/run.sh
+. "$(dirname "$0")"/run_container.sh
 
 docker exec -it "${CONTAINER_NAME}" /bin/zsh -c "source ~/.zshrc && \
                                                  source ${CONTAINER_HOME_FOLDER}/${CODE_FOLDER}/devel/setup.zsh && \
