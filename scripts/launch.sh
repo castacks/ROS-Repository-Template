fe17cc0e7e3ecced868c84e35401c57c69ee011f
Tom Notch <topnotchymk@gmail.com>
2024-11-18 12:14:41 -0500

    rename run.sh to avoid misunderstanding

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -2,7 +2,7 @@
 
 . "$(dirname "$0")"/variables.sh
 
-. "$(dirname "$0")"/run.sh
+. "$(dirname "$0")"/run_container.sh
 
 docker exec -it "${CONTAINER_NAME}" /bin/zsh -c "source ~/.zshrc && \
                                                  roslaunch new_package new_package_node.launch"
