8a396aac9757d41f1c6f7a54f1545297180dfda6
Tom Notch <topnotchymk@gmail.com>
2025-10-20 15:27:34 -0400

    Update template to easier instructions

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -5,5 +5,5 @@
 . "$(dirname "$0")"/run_container.sh
 
 docker exec -it "${CONTAINER_NAME}" /bin/zsh -c "source ~/.zshrc && \
-                                                 source ${CONTAINER_HOME_FOLDER}/${CODE_FOLDER}/devel/setup.zsh && \
+                                                 source ${HOME_FOLDER}/${CODE_FOLDER}/devel/setup.zsh && \
                                                  roslaunch new_package c++_node.launch"
